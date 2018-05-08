47a3dd1dd4efc2916a429b4a2f5e3bd04a5f056a
jemHerrera <jemPC@.github.com>
2018-05-08 15:21:40 -0600

    finished nav and banner

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    // BURGER MENU
+    let isBurger = false
+    $('.burger').on('click', function(){
+        if (!isBurger){
+            $('ul').css('width', '15em');
+            $('li').css('display', 'block');
+            $('.burger').children().css('margin', '0');
+            isBurger = true;
+        }
+        else{
+            $('ul').css('width', '0');
+            $('li').css('display', 'none');
+            $('.burger').children().css('margin', '5% 0');
+            isBurger = false;
+        }
+    });
+
+
+
+
+
+
+
+
+});
